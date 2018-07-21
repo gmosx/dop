a594c6090c11ca79e118dbcd6d6d15cabe21ac1f
George Moschovitis <gmosx@reizu.com>
2018-07-21 17:30:33 +0300

    Added --link option to ExecutableInstallCommand

--- Sources/Devops/Commands/InstallExecutableCommand.swift
+++ Sources/Devops/Commands/InstallExecutableCommand.swift
@@ -1,20 +1,27 @@
 import Utility
 
 // TODO: consider option to bump the version 
-// TODO: allow for `ln -sf` or `cp` 
-// ln -sf `pwd`/.build/release/\(project.executableName) /usr/local/bin/\(project.executableName)
 
 class InstallExecutableCommand: DevopsCommand {
+    var symbolicLinkOption: OptionArgument<Bool>!
+
     convenience init() {
         self.init(name: "executable-install", summary: "Install the executable to an in-path directory")
     }
 
+    override func setup() {
+        symbolicLinkOption = argumentParser.add(option: "--link", shortName: "-l", kind: Bool.self, usage: "Use symbolic link")
+    }
+
     override func run(result: ArgumentParser.Result) {
+        let shouldUseSymbolicLink = result.get(symbolicLinkOption) ?? false
+        let shellCommand = shouldUseSymbolicLink ? "ln -sf" : "cp -f"
+
         do {
             try shell.execute(script:
                 """
                 swift build -c release
-                cp -f .build/release/\(project.executableName) /usr/local/bin/\(project.executableName)
+                \(shellCommand) \(shell.currentDirectoryPath)/.build/release/\(project.executableName) /usr/local/bin/\(project.executableName)
                 """
             )
         } catch {
